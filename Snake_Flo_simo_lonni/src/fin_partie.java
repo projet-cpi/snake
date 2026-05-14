6e79e465334b8c60d5a8268114fb7e0eac7dcadf
Flo-flo67 <flo.puyf@hotmail.com>
2026-05-14 11:11:38 +0200

    Update fin_partie.java

--- Snake_Flo_simo_lonni/src/fin_partie.java
+++ Snake_Flo_simo_lonni/src/fin_partie.java
@@ -1,5 +1,39 @@
-
 public class fin_partie {
-  nhjjhhuyguygyg
 
+    // Défaite : le serpent sort de la grille
+    public static boolean collisionMur(Serpent serpent, Grille grille) {
+
+        int x = serpent.getHead()[0];
+        int y = serpent.getHead()[1];
+
+        if(x < 0 || x >= grille.getNblig()
+           || y < 0 || y >= grille.getNbCol()) {
+
+            System.out.println("GAME OVER");
+            return true;
+        }
+
+        return false;
+    }
+
+    // Victoire : taille max atteinte
+    public static boolean victoire(Serpent serpent, int tailleMax) {
+
+        if(serpent.getTaille() >= tailleMax) {
+
+            System.out.println("VICTOIRE");
+            return true;
+        }
+
+        return false;
+    }
+
+    // Vérification générale
+    public static boolean finJeu(Serpent serpent,
+                                 Grille grille,
+                                 int tailleMax) {
+
+        return collisionMur(serpent, grille)
+                || victoire(serpent, tailleMax);
+    }
 }
