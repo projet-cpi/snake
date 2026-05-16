3cabb07be7270830c049a53de7b6c1a99ec29e90
Flo-flo67 <flo.puyf@hotmail.com>
2026-05-16 19:44:03 +0200

    Update Affichage.java
    commentaire

--- Snake_Flo_simo_lonni/src/Affichage.java
+++ Snake_Flo_simo_lonni/src/Affichage.java
@@ -1,23 +1,34 @@
-
 public class Affichage {
-    private Grille grille ;
+    private Grille grille ; // La grille de jeu
+
+    // Constructeur : reçoit la grille en paramètre
     public Affichage(Grille grille) {
     	this.grille = grille;
     }
+
+    // Affiche la grille vide
     public void afficher() {
     	grille.afficher();
     }
-    public void afficher(Apple apple,Serpent serpent) {
+
+    // Affiche la grille avec la pomme et le serpent
+    public void afficher(Apple apple, Serpent serpent) {
         
-        grille.placer(serpent.getHead()[0],serpent.getHead()[1], 'T');
-        int[][]corps=serpent.getBody();
-        for(int i=0;i<serpent.getTaille();i++) {
-        	grille.placer(corps[i][0],corps[i][1], 'X');
+        // Place la tête du serpent ('T')
+        grille.placer(serpent.getHead()[0], serpent.getHead()[1], 'T');
+
+        // Récupère le corps du serpent
+        int[][] corps = serpent.getBody();
+
+        // Place chaque segment du corps ('X')
+        for(int i = 0; i < serpent.getTaille(); i++) {
+        	grille.placer(corps[i][0], corps[i][1], 'X');
         }
         
+        // Place la pomme ('P')
         grille.placer(apple.getX(), apple.getY(), 'P');
-
         
+        // Affiche la grille
         grille.afficher();
     }
 }
